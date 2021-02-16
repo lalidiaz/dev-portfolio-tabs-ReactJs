2133549c2b7b023bae25f1738402c0b3b54b6d1a
Lali <lauradiaz1586@gmail.com>
2021-02-16 21:59:58 +0400

    add state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,31 @@ import { FaAngleDoubleRight } from 'react-icons/fa';
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
-  return <h2>tabs project setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState([0]);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const newJobs = await res.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <h1>Loading ...</h1>
+      </section>
+    );
+  }
+
+  return <h2>jobs</h2>;
 }
 
 export default App;
