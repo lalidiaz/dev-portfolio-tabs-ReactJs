59837bbcd1100f444fedeaf3d0b5a6a4a46a488e
Lali <lauradiaz1586@gmail.com>
2021-02-17 14:04:02 +0400

    add styles and state

--- src/App.js
+++ src/App.js
@@ -27,8 +27,47 @@ function App() {
       </section>
     );
   }
+  const { company, dates, duties, title } = jobs[value];
 
-  return <h2>jobs</h2>;
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+      </div>
+      <div className="job-container">
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                type="button"
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((dutie, index) => {
+            return (
+              <div className="job-desc" key={index}>
+                <FaAngleDoubleRight className="icon" />
+                <p>{dutie}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  );
 }
 
 export default App;
